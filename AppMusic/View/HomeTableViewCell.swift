b039400647777f36d683817e069e0ff0a94105c3
gabrielaragaoo <eng.gabrielaragao@gmail.com>
2023-03-22 11:15:13 -0300

    feat: setup cell

--- AppMusic/View/HomeTableViewCell.swift
+++ AppMusic/View/HomeTableViewCell.swift
@@ -11,13 +11,41 @@ class HomeTableViewCell: UITableViewCell {
 
     static let identifier = "HomeTableViewCellIdentifier"
 
+    private lazy var cardView: CardView = {
+        let card = CardView()
+        card.translatesAutoresizingMaskIntoConstraints = false
+        card.clipsToBounds = true
+        return card
+    }()
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        setupView()
 
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    } 
+    }
+
+    func setupCell(data: CardViewModel) {
+        cardView.setupCardView(data: data)
+    }
+
+    private func setupView() {
+        contentView.addSubview(cardView)
+
+        setupConstraints()
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            cardView.topAnchor.constraint(equalTo: topAnchor),
+            cardView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            cardView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            cardView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
 
 }
