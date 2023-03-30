b1d1885bee2592a83610194a6f9cb713f569046b
gabrielaragaoo <eng.gabrielaragao@gmail.com>
2023-03-30 09:47:07 -0300

    feat: custom nav bar

---
+++ AppMusic/View/CustomNavBar.swift
@@ -0,0 +1,66 @@
+//
+//  CustomNavBar.swift
+//  AppMusic
+//
+//  Created by Gabriel Aragao on 30/03/23.
+//
+
+import UIKit
+
+class CustomNavBar: UIView {
+
+    private lazy var categoryTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 12, weight: .bold)
+        label.textAlignment = .center
+        label.textColor = .white
+        return label
+    }()
+
+    private lazy var musicTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.textAlignment = .center
+        label.textColor = .white
+        return label
+    }()
+
+    private lazy var likeAndTimeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 8, weight: .regular)
+        label.textAlignment = .center
+        label.textColor = .white
+        return label
+    }()
+
+    private lazy var cardImage: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .scaleToFill
+        image.clipsToBounds = true
+        return image
+    }()
+
+    private lazy var overlayView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .black
+        view.alpha = 0.6
+        return view
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
